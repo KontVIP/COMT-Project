437fc7a3c96dc21c5054b95def4a5d3d1b65b779
KontVIP <76660306+KontVIP@users.noreply.github.com>
2021-04-16 00:45:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/stylishclothes/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.stylishclothes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_catalog:
+                            selectedFragment = new CatalogFragment();
+                            break;
+                        case R.id.nav_shopping_cart:
+                            selectedFragment = new ShoppingCartFragment();
+                            break;
+                        case R.id.nav_favorites:
+                            selectedFragment = new FavoritesFragment();
+                            break;
+                        case R.id.nav_menu:
+                            selectedFragment = new MenuFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+}
